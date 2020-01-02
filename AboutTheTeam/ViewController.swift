36dfcd1054b9e68c61240167ba4d8c9f21b5047e
Aaron Shackelford <burntcreamlatte@icloud.com>
2020-01-02 10:04:37 -0700

    Initial Commit

---
+++ AboutTheTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutTheTeam
+//
+//  Created by Aaron Shackelford on 1/2/20.
+//  Copyright © 2020 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
